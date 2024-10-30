7f66ef6da2c1575f94d4162408e108e196f57c9d
Toby Liu <ybot1122@gmail.com>
2024-10-30 12:04:04 -0700

    error page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import Root from "./root.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import ErrorPage from "./error-page.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
---
+++ src/root.tsx
@@ -0,0 +1,9 @@
+function Root() {
+  return (
+    <>
+      <div className="text-xl font-bold">I am here</div>
+    </>
+  );
+}
+
+export default Root;
